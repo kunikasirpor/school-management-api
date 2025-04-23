41e4901a1a8665444d0c49d45eff778b1024dddd
Kunika <157152440+kunikasirpor@users.noreply.github.com>
2025-04-23 22:39:36 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 // db.js
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host:     process.env.MYSQL_HOST,
+  port:     process.env.MYSQL_PORT,
+  user:     process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 export default pool;
